d3a8b896c9dba02686d608b63bfdbd1d8548ad9b
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-01 13:17:59 -0500

    Agrego la ruta Doctor

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const routesUser = require('./users')
+const routesDoctor = require('./doctor')
 const dummyRoutes = require('../../dummyData/dummyRoutes/index.routes');
 
 /* GET home page. */
 router.use('/users',routesUser );
 
+/* Doctor routes */
+router.use('/doctors', routesDoctor);
+
 /* GET dummy routes. */
 router.use('/dummy',dummyRoutes );
+
 module.exports = router;
