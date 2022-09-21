530f7daa45539d59a2edbab105e5f67a249b2fc1
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-21 16:50:23 -0500

    Cambios pequeños estilos responsive

--- client/src/Vistas/Compartido/Componentes/Footer/Footer.js
+++ client/src/Vistas/Compartido/Componentes/Footer/Footer.js
@@ -3,45 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="text-center  bg-[#292F53] pt-2 pb-2 border-[1px] border-gray-200">
+    <footer className="font-raleway flex flex-col text-[#292F53] text-center m-5 py-3 border-t border-[#292f5333]">
       <div>
-        <aside class="   mt-[20px] w-[100%] rounded p-2">
-          <div className="flex flex-row m-[25px] justify-center  ">
-            <Link
-              className="text-blue-500 hover:text-blue-400 font-poppins mr-[35px] ml-[35px] text-[25px]"
-              to="/servicios"
-            >
-              Servicios
-            </Link>
-
-            <Link
-              className="text-blue-500 hover:text-blue-400 font-poppins mr-[35px] ml-[35px] text-[25px]"
-              to="/somos"
-            >
-              A que nos dedicamos
-            </Link>
-
-            <Link
-              className="text-blue-500 hover:text-blue-400 font-poppins mr-[35px] ml-[35px] text-[25px]"
-              to="/contacto"
-            >
-              Contactanos
-            </Link>
-
-            <Link
-              className="text-blue-500 hover:text-blue-400 font-poppins mr-[35px] ml-[35px] text-[25px]"
-              to="/about"
-            >
-              Acerca de nosotros
-            </Link>
-          </div>
-        </aside>
-      </div>
-
-      <hr className="my-3" />
-      <div>
-        <p className=" text-white">
-          www.MedicApp.com © 2022 - Encontrá tu especialista y pedí turno
+        <Link className="text-[#1479FF] hover:underline" to="/about">
+          Sobre Nosotros
+        </Link>
+        <p className="">
+          www.MedicApp.com © 2022 - Encontrá tu especialista y pedí tu turno
         </p>
       </div>
     </footer>
--- client/src/Vistas/Compartido/Componentes/Header/NavBar.js
+++ client/src/Vistas/Compartido/Componentes/Header/NavBar.js
@@ -13,21 +13,21 @@ export default function NavBar ({avaliable}){
 
     return (
         <div className=''>
-            <div className="flex flex-row justify-center md:justify-between bg-[#E7EFFD] px-12 py-2">
+            <div className="flex flex-row justify-between bg-[#E7EFFD] md:mx-5 px-5 py-3 border-b border-[#292f5333]">
             <Link to = {user?.rol === 'ADMIN' ? '/admin/home' : user?.rol === 'DOCTOR' ? '/doctor/home' : user?.rol === 'PATIENT' ? '/patient/home' : '/'}>
-            <img className="object-contain w-16 hidden md:block" src={imagen} alt='logo'/>
+            <img className="object-contain w-16" src={imagen} alt='logo'/>
             </Link>
             <div className="flex items-center">
             
             {
                 user ? 
-                <div className="flex items-center mx-2 gap-2">
+                <div className="flex font-raleway items-center mx-2 gap-2">
                     <div className="flex flex-col items-end justify-center gap-1"> 
-                        <p className="text-xs font-poppins">{user?.email}</p>
-                        <p className="text-xs font-semibold opacity-75">Rol: {user?.rol}</p>
+                        <p className="text-xs">{user?.email}</p>
+                        <p className="text-xs opacity-75">Rol: {user?.rol}</p>
                     </div>
                     <Link to={user?.rol === 'ADMIN' ? '/admin' : user?.rol === 'DOCTOR' ? '/doctor' : user?.rol === 'PATIENT' ? '/patient' : null}>
-                        <p className="bg-[#1479FF] hover:bg-[#292F53] font-poppins text-white py-2 px-4 rounded">Panel</p>
+                        <p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none ">Panel</p>
                     </Link>
 
                 </div>
@@ -37,27 +37,26 @@ export default function NavBar ({avaliable}){
             {
                 JSON.parse(session) ? 
                         user.rol === 'ADMIN' ?
-                    <Link to={LOGOUT_ADMIN}><p className="bg-[#292F53] hover:bg-[#1479FF] font-poppins text-white py-2 px-4 rounded">Logout</p> </Link>
+                    <Link to={LOGOUT_ADMIN}><p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none ">Logout</p> </Link>
                     :  user.rol === 'DOCTOR' ?
-                    <Link to={LOGOUT_DOCTOR}> <p className="bg-[#292F53] hover:bg-[#1479FF] font-poppins text-white py-2 px-4 rounded">Logout</p></Link>
+                    <Link to={LOGOUT_DOCTOR}> <p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none ">Logout</p></Link>
                     :   user.rol === 'PATIENT'?
-                    <Link to={LOGOUT_PATIENT}><p className="bg-[#292F53] hover:bg-[#1479FF] font-poppins text-white py-2 px-4 rounded">Logout</p> </Link> 
+                    <Link to={LOGOUT_PATIENT}><p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none ">Logout</p> </Link> 
                     : 
                     window.location.href !== "http://localhost:3000/login"?
                     <Link to ='/login'>
-                        <p className="bg-[#1479FF] hover:bg-[#292F53] font-poppins text-white py-2 px-4 rounded">Ingresar</p>
+                        <p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none">Ingresar</p>
                     </Link>
                     :<></>
                     : window.location.href !== "http://localhost:3000/login" ?
                     <Link to ='/login'>
-                        <p className="bg-[#1479FF] hover:bg-[#292F53] font-poppins text-white py-2 px-4 rounded">Ingresar</p>
+                        <p className="text-white w-full py-2 px-4 font-poppins md:text-sm text-xs rounded-lg bg-[#292F53] hover:bg-[#1479FF] focus:outline-none ">Ingresar</p>
                     </Link>
                     :<></>
             }
             
             </div>
             </div>
-            <hr className='border-solid border-1 border-gray-500 opacity-20'></hr>
             
             
             {/* <div className="flex items-center space-x-10 ">
